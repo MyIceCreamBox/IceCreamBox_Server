ad87ceeb5b9b51b4de6f47f28535975b6a5523ff
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2023-04-28 13:04:11 +0900

    프로젝트 세팅

---
+++ src/main/java/com/example/myicecreambox/MyIceCreamBoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.myicecreambox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyIceCreamBoxApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MyIceCreamBoxApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/myicecreambox/MyIceCreamBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myicecreambox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyIceCreamBoxApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
