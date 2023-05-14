ebf9c090f547d2033c6ab4d81542e5610f994abb
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2023-05-14 17:10:00 +0900

    #1 feat: 유저 API 기능구현 & jwt 토큰 구현 & loginResolver 구현

--- src/main/java/com/example/myicecreambox/user/repository/UserRepository.java
+++ src/main/java/com/example/myicecreambox/user/repository/UserRepository.java
@@ -4,7 +4,15 @@ import com.example.myicecreambox.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findByUserIdxAndIsEnable(Long userIdx, Boolean isEnable);
 
+  User findByEmailAndPw(String email, String pw);
+
+  Boolean existsByNickname(String nickName);
+
+  Boolean existsByEmail(String email);
 }
