b3484143f5a8ccc4439940f4837786c83d651694
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2023-05-21 18:14:15 +0900

    #9 feat: gift api 뼈대만들기(controller, service layer interface 작성)

---
+++ src/main/java/com/example/myicecreambox/gift/service/GiftServiceImpl.java
@@ -0,0 +1,14 @@
+package com.example.myicecreambox.gift.service;
+
+import com.example.myicecreambox.gift.dto.SendGiftReq;
+import com.example.myicecreambox.gift.dto.SendGiftRes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GiftServiceImpl implements GiftService {
+
+  @Override
+  public SendGiftRes sendGift(SendGiftReq sendGiftReq, Long userIdx) {
+    return null;
+  }
+}
--- src/main/java/com/example/myicecreambox/gift/service/MessageService.java
+++
@@ -1,4 +0,0 @@
-package com.example.myicecreambox.gift.service;
-
-public interface MessageService {
-}
